0af0b4c127160c22875664b845898d495ede2616
Evan <apexdigitalminds@gmail.com>
2025-11-22 11:26:13 +0800

    FIX: Add required placeholder props to AppProvider wrapper in the Collection route

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -6,12 +6,17 @@ import ProtectedRoute from '../components/bolt/ProtectedRoute';
 import Layout from '../components/bolt/Layout';
 import { AppProvider } from '@/context/AppContext';
 
+const PLACEHOLDER_ID = "collection-placeholder";
+
 export default function Page() {
   return (
-    <AppProvider>
+<AppProvider 
+        verifiedUserId={PLACEHOLDER_ID} 
+        experienceId={PLACEHOLDER_ID}
+    >
       <Layout>
         <ProtectedRoute>
-          <BadgeShowcase />
+          <BadgeShowcase /> 
         </ProtectedRoute>
       </Layout>
     </AppProvider>
