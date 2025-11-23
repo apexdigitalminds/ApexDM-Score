f7ff6f656e92500e7ebda35ce486d16591257f93
Evan <apexdigitalminds@gmail.com>
2025-11-23 09:52:56 +0800

    FIX: Remove local AppProvider from Profile route to stop creating [userid]-placeholder

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,25 +1,17 @@
 "use client";
 
 import React from 'react';
-// Use ../../../ because we are deep in [userId]
 import ProfilePage from '../../components/bolt/ProfilePage';
 import ProtectedRoute from '../../components/bolt/ProtectedRoute';
 import Layout from '../../components/bolt/Layout';
-import { AppProvider } from '@/context/AppContext';
-
-const PLACEHOLDER_ID = "[userid]-placeholder";
 
 export default function Page() {
   return (
-<AppProvider 
-        verifiedUserId={PLACEHOLDER_ID} 
-        experienceId={PLACEHOLDER_ID}
-    >
-      <Layout>
-        <ProtectedRoute>
-          <ProfilePage />
-        </ProtectedRoute>
-      </Layout>
-    </AppProvider>
+    // FIX: Removed AppProvider wrapper. Using global context from layout.tsx
+    <Layout>
+      <ProtectedRoute>
+        <ProfilePage />
+      </ProtectedRoute>
+    </Layout>
   );
 }
