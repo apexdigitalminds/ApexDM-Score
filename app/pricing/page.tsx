bb10d6290e8f08d6550e119b627b8a42b09c57db
Evan <apexdigitalminds@gmail.com>
2025-12-02 21:56:48 +0800

    pricing layout

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,24 +3,13 @@
 import React from 'react';
 // Import path is '../' because we are in 'app/pricing'
 import PricingPage from '../components/bolt/PricingPage';
-import Layout from '../components/bolt/Layout';
-import { AppProvider } from '@/context/AppContext';
-
-const PLACEHOLDER_ID = "public-pricing";
 
 export default function Page() {
   return (
-<AppProvider 
-        verifiedUserId={PLACEHOLDER_ID} 
-        experienceId={PLACEHOLDER_ID}
-    >
-      <Layout>
-        {/* We do NOT wrap this in ProtectedRoute. 
-          This allows public visitors (and potential customers) to see your pricing 
-          before they sign up. The Layout will handle showing "Sign In" vs "User Menu" correctly.
-        */}
-        <PricingPage />
-      </Layout>
-    </AppProvider>
+    /* 🟢 FIX: Removed <AppProvider> and <Layout>. 
+      This page is already wrapped by the Root Layout in app/layout.tsx.
+      Adding them again caused the "Double Header" and session loss issue.
+    */
+    <PricingPage />
   );
 }
